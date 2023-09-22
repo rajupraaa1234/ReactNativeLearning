3bf29f73861d8a24c49d2fa5bd3d23aaeffb1602
rajukumar <raju.kumaer@ei.study>
2023-09-23 02:35:14 +0530

    Done

--- ReactNativeLearnings/Mycomponent.tsx
+++ ReactNativeLearnings/Mycomponent.tsx
@@ -1,9 +1,60 @@
 import React, { useEffect } from "react";
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Alert } from 'react-native';
 import notifee from '@notifee/react-native';
+import messaging from '@react-native-firebase/messaging';
 
 
 const MyComponent = () => {
+
+    useEffect(() => {
+        checkToken();
+        notification();
+        requestUserPermission();
+
+        //This is for ForeGround 
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+            Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+        });
+        return unsubscribe;
+    }, []);
+
+
+    const checkToken = async () => {
+        const fcmToken = await messaging().getToken();
+        if (fcmToken) {
+            console.log(fcmToken);
+        }
+    }
+
+    async function requestUserPermission() {
+        const authStatus = await messaging().requestPermission();
+        const enabled =
+            authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+            authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+        if (enabled) {
+            console.log('Authorization status:', authStatus);
+        }
+    }
+
+    const notification = () => {
+        messaging().onNotificationOpenedApp(remoteMessage => {
+            console.log(
+                'Notification caused app to open from background state:',
+                remoteMessage.notification,
+            );
+        });
+        messaging()
+            .getInitialNotification()
+            .then(remoteMessage => {
+                if (remoteMessage) {
+                    console.log(
+                        'Notification caused app to open from quit state:',
+                        remoteMessage.notification,
+                    );
+                }
+            });
+    }
 
     const getLocalNotification = async () => {
         // Request permissions (required for iOS)
