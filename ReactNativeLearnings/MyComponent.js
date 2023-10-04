33e494318f2be3366326754c9d98aa484e7dfc40
rajukumar <raju.kumaer@ei.study>
2023-10-05 01:24:43 +0530

    Throting done

--- ReactNativeLearnings/MyComponent.js
+++ ReactNativeLearnings/MyComponent.js
@@ -33,10 +33,29 @@ const MyComponent = () =>{
             callApi();
          },3000);
    }
+
+   // In every 2 sec Api will call of typing is still going on by the user . 
+   const throtling = () =>{
+      let flag = true;
+      return function(){
+        if(flag){
+            flag = false;
+            callApi();
+            setTimeout(()=>{
+                flag = true;
+            },2000)
+        }
+      }
+   }
+   const Throtling = throtling();
+   const throtlingCheck = (text) =>{
+         setName(text);
+         Throtling();
+   }
     return (
         <View style={{justifyContent:'center',flex:1,alignSelf:'center'}}>
             <Text>Raju kumar</Text>
-            <TextInput value={name} onChange={withClosure} style={{height : 40,width:320,borderColor:'gray',borderWidth:2,marginTop:20}}/>
+            <TextInput value={name} onChange={throtlingCheck} style={{height : 40,width:320,borderColor:'gray',borderWidth:2,marginTop:20}}/>
         </View>
     )
 }
