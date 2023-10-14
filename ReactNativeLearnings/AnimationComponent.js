e1d2339ddac8b4a520fba7147a7f5adef585180e
rajukumar <raju.kumaer@ei.study>
2023-10-15 02:27:19 +0530

    spring aniumation

--- ReactNativeLearnings/AnimationComponent.js
+++ ReactNativeLearnings/AnimationComponent.js
@@ -4,9 +4,18 @@ import {View , Text , Animated } from 'react-native';
 
 const AnimationComponent = () =>{
     const position = new Animated.ValueXY({x:0,y:0});
-    Animated.timing(position,{              // use timing method for linear movement in x & y axis .
+    // Animated.timing(position,{              // use timing method for linear movement in x & y axis .
+    //     toValue : {x:200,y:500},
+    //     useNativeDriver : true,              // use this for smoth move 
+    //     duration : 5000                      // duration to complete an animation .
+    // }).start()                              // need to start animation using start method . 
+
+
+    Animated.spring(position,{              // use timing method for linear movement in x & y axis .Animates a value according to an analytical spring model based on damped harmonic oscillation.
         toValue : {x:200,y:500},
-        useNativeDriver : true              // use this for smoth move 
+        useNativeDriver : true,              // use this for smoth move 
+        speed : 20,                           // speed of animation 
+        bounciness : 40                       // for give bounice 
     }).start()                              // need to start animation using start method . 
     return (
         <View style={{flex:1}}>
@@ -15,7 +24,8 @@ const AnimationComponent = () =>{
                 width:80,
                 backgroundColor:'red',
                 transform : [               // add transform ( aray which take set of object or animation )
-                    {translateX : position.x}
+                    {translateX : position.x},
+                    {translateY : position.y}
                 ]
              }}>
 
